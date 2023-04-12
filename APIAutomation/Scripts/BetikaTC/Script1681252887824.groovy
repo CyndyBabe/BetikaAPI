dd3283bad2043ccc54d309ab7c6a08ab5773fa3d
CyndyBabe <130548188+CyndyBabe@users.noreply.github.com>
2023-04-12 15:53:28 +0200

    Add files via upload

---
+++ APIAutomation/Scripts/BetikaTC/Script1681252887824.groovy
@@ -0,0 +1,40 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.logging.KeywordLogger
+
+
+//
+'Request and Response'
+def response = WS.sendRequest(findTestObject('Object Repository/Betika'));
+
+
+'2.Verify the api has a response code of 200'
+def status = WS.verifyResponseStatusCode(response, 200)
+
+
+'3.Verify ‘User 6’ is present in the response'
+WS.verifyElementPropertyValue(response, '[5].userName', 'User 6');
+
+'4. Number of Objects' 
+def objectCount = WS.getElementsCount(response, 'userName')
+
+WebUI.comment("There are - "+ objectCount +" objects in the api response");
+
+
+
---
+++ APIAutomation/Test Suites/Betika-TestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Betika-TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d400c06c-05d8-4f20-93b5-c6a928399254</testSuiteGuid>
+   <testCaseLink>
+      <guid>fc4450f1-cb5c-4430-a0d3-d009e57359dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BetikaTC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
